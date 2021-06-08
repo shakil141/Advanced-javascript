aecd17e843c065e066f2b0cb3aaf12758bae2b4c
shakil141 <fcishakil@gmail.com>
2021-06-08 17:17:46 +0600

    ture vs false

---
+++ true-false.js
@@ -0,0 +1,51 @@
+// const age = 14;
+
+// if(age >=18){
+//     console.log("The man is adult");
+// }else{
+//     console.log("The man is young");
+// }
+
+// const age = 4;
+
+//     if(age <0){
+//         console.log("The Condition is true");
+//     }else{
+//         console.log("The Condition is false"); 
+//     }
+
+
+// Truthy values
+// true
+// "0" , " "
+// 111
+// "false"
+// {} ,[]
+// function(){} empty function
+
+
+
+
+
+// falsy values
+
+// false
+// '' & "" empty string
+// undefined
+// NULL
+// NaN
+// 0 
+
+
+
+// const name = "shakil";
+
+// if(name){
+//     console.log("This is true");
+// }else{
+//     console.log("this is false");
+// }
+
+    let a = false;
+    console.log(a)
+    console.log(Boolean(a))
