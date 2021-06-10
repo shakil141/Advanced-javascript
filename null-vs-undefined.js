d4996430e391dd415be98c5f4e9db3ceb47c6a72
shakil141 <fcishakil@gmail.com>
2021-06-10 08:53:26 +0600

    null vs undefined for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+    // NULL vs Undefined
+
+    //let abc;
+    //let abc = null;
+    //console.log(abc);
+
+    //console.log(abc);
+
+    // function add(num1,num2)
+    // {
+    //     console.log(num1,num2);
+
+    // }
+
+    // add(10);
+
+    
